046f7e0b46e3947265895e9e9f55c9fcd4fd0ed6
ttrinhquang <43072505+ttrinhquang@users.noreply.github.com>
2024-07-27 22:07:19 +0700

    clean some folders

--- src/test/java/driver/MobileCapabilityType.java
+++
@@ -1,9 +0,0 @@
-package driver;
-
-public interface MobileCapabilityType {
-    String PLATFORM_NAME = "platformName";
-    String AUTOMATION_NAME = "automationName";
-    String UDID = "udid";
-    String APP_PACKAGE = "appPackage";
-    String APP_ACTIVITY = "appActivity";
-}
--- src/test/java/driver/Platform.java
+++
@@ -1,5 +0,0 @@
-package driver;
-
-public enum Platform {
-    ANDROID, IOS;
-}
